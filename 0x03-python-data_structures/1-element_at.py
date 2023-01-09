92bc33e7553336b8b67c540f4f000ccbe47a4f81
Agbeniga Agboola <agboolaagbeniga@gmail.com>
2023-01-09 15:56:13 -0800

    a credible commit

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,11 @@
+nes (9 sloc)  232 Bytes
+
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    for x in my_list:
+        if idx == my_list.index(x):
+            return(x)
+        elif idx > len(my_list):
+            return None
+        elif idx < 0:
+            return None
