a1cacc68f0520d230170059d38e1a7edfb27d4ae
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2023-05-11 20:56:17 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import hidden_4
     names = [name for name in dir(hidden_4) if not name.startswith('__')]
     for name in sorted(names):
-       print(name)
+        print(name)
