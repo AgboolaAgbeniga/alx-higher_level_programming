f56d122a1d48001832a4e6d012641c32e594bc2f
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2023-05-11 13:19:20 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-# importing the function from add_0.py
-from add_0 import add
+if __name__ == "__main__":
+  # importing the function from add_0.py
+  from add_0 import add
 
-# assigning values to a and b
-a = 1
-b = 2
+  # assigning values to a and b
+  a = 1
+  b = 2
 
-# calling the add function with arguments a and b, and printing the result
-print('{} + {} = {}'.format(a, b, add(a, b)))
-
+  # calling the add function with arguments a and b, and printing the result
+  print('{} + {} = {}'.format(a, b, add(a, b)))
