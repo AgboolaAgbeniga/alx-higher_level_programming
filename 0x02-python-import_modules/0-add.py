0383874de6efb84c54a16cdc6561d0cd4bf2ef3c
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2023-05-11 19:31:54 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-  """print function with string format to display integers"""
-  from add_0 import add
+    """print function with string format to display integers"""
+    from add_0 import add
 
-  a = 1
-  b = 2 
-  print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
