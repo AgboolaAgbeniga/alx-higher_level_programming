d755e7888586553d2085f52f9cb3e2f132c10909
AgboolaAgbeniga <114500552+AgboolaAgbeniga@users.noreply.github.com>
2023-05-18 02:17:42 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-
 def multiply_by_2(a_dictionary):
-    # Create a new dictionary to store the multiplied values
-    new_dictionary = {}
-
-    # Iterate over the key-value pairs in the input dictionary
-    for key, value in a_dictionary.items():
-        # Multiply the value by 2 and store it in the new dictionary
-        new_dictionary[key] = value * 2
-
-    return new_dictionary
+    New_dic = {}
+    for i in a_dictionary:
+        Newvalue = (a_dictionary.get(i)) * 2
+        New_dic.update({i: Newvalue})
+    return (New_dic)
